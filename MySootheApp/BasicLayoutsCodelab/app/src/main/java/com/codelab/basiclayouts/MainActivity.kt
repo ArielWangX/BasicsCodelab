02412add517bc4c76e76e13eae35df2b590e0411
Ariel Wang <arielxiewang@gmail.com>
2022-08-26 15:46:10 +1000

    Bottom Navigation - Material

--- MySootheApp/BasicLayoutsCodelab/app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
+++ MySootheApp/BasicLayoutsCodelab/app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
@@ -32,7 +32,9 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Spa
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -206,8 +208,8 @@ fun HomeSection(
 fun HomeScreen(modifier: Modifier = Modifier) {
     Column(
         modifier = Modifier
-            . verticalScroll(rememberScrollState())
-            . padding(vertical = 16.dp)
+            .verticalScroll(rememberScrollState())
+            .padding(vertical = 16.dp)
     ) {
 //        Spacer(modifier = Modifier.height(16.dp))
         SearchBar()
@@ -224,7 +226,36 @@ fun HomeScreen(modifier: Modifier = Modifier) {
 // Step: Bottom navigation - Material
 @Composable
 private fun SootheBottomNavigation(modifier: Modifier = Modifier) {
-    // Implement composable here
+    BottomNavigation(
+        backgroundColor = MaterialTheme.colors.background,
+        modifier = Modifier) {
+        BottomNavigationItem(
+            icon = {
+                   Icon(
+                       imageVector = Icons.Default.Spa,
+                       contentDescription = null
+                   )
+            },
+            label = {
+                    Text(stringResource(id = R.string.bottom_navigation_home))
+            },
+            selected = true,
+            onClick = {}
+        )
+        BottomNavigationItem(
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.AccountCircle,
+                    contentDescription = null
+                )
+            },
+            label = {
+                    Text(stringResource(id = R.string.bottom_navigation_profile))
+            },
+            selected = false,
+            onClick = {}
+        )
+    }
 }
 
 // Step: MySoothe App - Scaffold
